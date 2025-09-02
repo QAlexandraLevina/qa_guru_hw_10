9b5adb706e58e9eb168192a67f502eb369390fd4
Alexandra Levina <93614091+Drumlex17@users.noreply.github.com>
2025-09-02 19:50:55 +0300

    add tests

--- tests/test_attachments.py
+++ tests/test_attachments.py
@@ -1,40 +1,10 @@
+import json
+
 import allure
-from selene.support import by
-from selene.support.conditions import be
-from selene.support.shared import browser
-from selene.support.shared.jquery_style import s
+from allure import attachment_type
 
 
-def test_decorator_steps():
-    open_main_page()
-    search_for_repository("eroshenkoam/allure-example")
-    go_to_repository("eroshenkoam/allure-example")
-    open_actions_tab()
-    should_see_actions_with_number("#281")
-
-
-@allure.step("Открытие главной страницы GitHub")
-def open_main_page():
-    browser.open("https://github.com")
-
-
-@allure.step("Поиск репозитория {repo}")
-def search_for_repository(repo):
-    s(".search-input").click()
-    s("#query-builder-test").send_keys(repo)
-    s("#query-builder-test").submit()
-
-
-@allure.step("Переход в репозиторий")
-def go_to_repository(repo):
-    s(by.link_text(repo)).click()
-
-
-@allure.step("Переход во вкладку 'Actions'")
-def open_actions_tab():
-    s("#actions-tab").click()
-
-
-@allure.step("Проверка наличия 'Actions' под номером {number}")
-def should_see_actions_with_number(number):
-    s(by.partial_text(number)).should(be.visible)
+def test_attachments():
+    allure.attach("Text content", name="Content from text", attachment_type=attachment_type.TEXT)
+    allure.attach("<h1>Hello, world<h1>", name="Html", attachment_type=attachment_type.HTML)
+    allure.attach(json.dumps({"first": 1, "second": 2}), name='json', attachment_type=attachment_type.JSON)
--- tests/test_decorators_steps.py
+++ tests/test_decorators_steps.py
@@ -5,20 +5,36 @@ from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
 
 
-def test_dynamic_steps():
-    with allure.step("Открытие главной страницы GitHub"):
-        browser.open("https://github.com")
+def test_decorator_steps():
+    open_main_page()
+    search_for_repository("eroshenkoam/allure-example")
+    go_to_repository("eroshenkoam/allure-example")
+    open_actions_tab()
+    should_see_actions_with_number("#281")
 
-    with allure.step("Поиск репозитория"):
-        s(".search-input").click()
-        s("#query-builder-test").send_keys("eroshenkoam/allure-example")
-        s("#query-builder-test").submit()
 
-    with allure.step("Переход в репозиторий"):
-        s(by.link_text("eroshenkoam/allure-example")).click()
+@allure.step("Открытие главной страницы GitHub")
+def open_main_page():
+    browser.open("https://github.com")
 
-    with allure.step("Переход во вкладку 'Actions'"):
-        s("#actions-tab").click()
 
-    with allure.step("Проверка наличия 'Actions' под номером '281'"):
-        s(by.partial_text("#281")).should(be.visible)
+@allure.step("Поиск репозитория {repo}")
+def search_for_repository(repo):
+    s(".search-input").click()
+    s("#query-builder-test").send_keys(repo)
+    s("#query-builder-test").submit()
+
+
+@allure.step("Переход в репозиторий")
+def go_to_repository(repo):
+    s(by.link_text(repo)).click()
+
+
+@allure.step("Переход во вкладку 'Actions'")
+def open_actions_tab():
+    s("#actions-tab").click()
+
+
+@allure.step("Проверка наличия 'Actions' под номером {number}")
+def should_see_actions_with_number(number):
+    s(by.partial_text(number)).should(be.visible)
